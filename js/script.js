1af12fbcd383ef9afed46f39617ad974db42c057
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-07-18 19:55:51 -0300

    more updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ativar from './modules/ativar.js';
-// import styleClick from './modules/styleClick.js';
+import styleClick from './modules/styleClick.js';
 
 ativar();
-// styleClick();
+styleClick();
